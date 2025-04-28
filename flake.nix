ee249ef4eaed29fdae7b4cc4460ca0d63e261ed9
Eduardo Eidelwein Berlitz <eberlitz@gmail.com>
2025-04-28 09:11:30 -0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Example nix-darwin system flake";
+  description = "nix-darwin configs for my machines";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
